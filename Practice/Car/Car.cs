9fd269a4d2014f687e645bb211b291e32baee092
David <davidamadorc@gmail.com>
2023-05-01 13:40:15 -0600

    Practice exercises

---
+++ Practice/Car/Car.cs
@@ -0,0 +1,20 @@
+﻿public class Car{
+
+    public string brand;
+    public string year;
+    public int mileage;
+    public bool lights = false;
+
+    public Car(){
+
+    }
+
+    public void displayInfo(){
+        Console.WriteLine($"Brand: {brand}, year: {year}, mileage: {mileage}, lights: {lights}.");
+    }
+
+    public void turnOn(){
+        lights = true;
+
+    }
+}
---
+++ Practice/Car/Program.cs
@@ -0,0 +1,7 @@
+Car bmw_m3 = new Car();
+bmw_m3.brand = "BMW";
+bmw_m3.year = "2023";
+bmw_m3.mileage = 5000;
+
+bmw_m3.turnOn();
+bmw_m3.displayInfo();
